abdd88126dcec9e449eaf22e4fd93d0ded7787e5
Vincenza Paradiso <122514035+vinniparadiso@users.noreply.github.com>
2023-01-27 11:38:48 -0500

    Add files via upload

---
+++ Paradiso Quiz 1.py
@@ -0,0 +1,9 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Problem 4
+2+(5-3)**5*27/(4*3)
+74.0
+#Problem 5
+print ("Be not afraid of greatness. \nSome are born great, some achieve greatness, and others have greatness thrust upon them.")
+Be not afraid of greatness. 
+Some are born great, some achieve greatness, and others have greatness thrust upon them.
